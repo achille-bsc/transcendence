67f7c243b0dc622fe35b54beb6d6c2eb83746f0d
ameduboi <ameduboi@student.42lehavre.fr>
2025-12-18 18:10:21 +0100

    Frontend(start) : first commit
    Sharing the beginning of the frontend part

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent';
+      violet: #6E3CA3;
+      red: #A82828;
+      white: #ffffffff;
+    }
+  },
+  plugins: [],
+};
