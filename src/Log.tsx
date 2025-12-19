f5516248743b3779da7437e7a09bebd823142f41
ameduboi <ameduboi@student.42lehavre.fr>
2025-12-19 15:55:29 +0100

    Upgrade: Login page
    Improved the login page interface setup colors

--- src/Log.tsx
+++ src/Log.tsx
@@ -4,15 +4,19 @@ function Log () {
 
 	return (
 		<>
-			<div>
-				<form action="URL">
-					<label htmlFor="">
-						<input type="text" class="border m-[15px]" placeholder="name" required/>
+			<div class="flex min-h-screen justify-center items-center">
+				<form action="URL" class="space-y-3 border px-25 p-21 bg-[#282828]">
+					<label>
+						<input type="text" class="border bg-[#3A3A3A] placeholder-[#9B9B9B]" placeholder="name" required/>
 					</label><br />
-					<label htmlFor="">
-						<input type="password" class="border m-[15px]" placeholder="password" required/>
-					</label><br />
-					<input type="submit" value="tkt fait confiance" class="border"/>
+					<div class="space-y-5">
+						<label>
+							<input type="password" class="border bg-[#3A3A3A] placeholder-[#9B9B9B]" placeholder="password" required/>
+						</label><br />
+						<div class="flex justify-center items-center">
+							<input type="submit" value="Register" class=" p-2 px-5 border border-[#6E3CA3] bg-[#3A3A3A] text-[#9B9B9B]"/>
+						</div>
+					</div>
 				</form>
 			</div>
 		</>
