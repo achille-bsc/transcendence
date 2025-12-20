ae146c23883975d4271867dbd258bd50050faab8
alegrix <16al07@gmail.com>
2025-12-20 03:03:12 +0100

    all

---
+++ dist/src/global_chat.js
@@ -0,0 +1,17 @@
+import { prisma } from '../prisma';
+async function newGlobalMessage(user_id, content) {
+    const new_message = await prisma.globalChat.create({
+        data: {
+            userId: user_id,
+            message: content
+        }
+    });
+    await prisma.user.update({
+        where: { id: user_id },
+        data: {
+            message_history: {
+                connect: { id_message: new_message.id_message },
+            },
+        },
+    });
+}
--- src_serveur/serveur.ts
+++ src_serveur/serveur.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
+import { prisma, } from '../prisma'
+import { getProfile } from './routes/auth'
+import { createUser, signinValidations } from './routes/signin'
+import { Prisma } from '../generated/prisma/client'
+import { generateToken } from './utils'
 import { findProfile } from '../src/script'
-import { prisma } from '../prisma'
-import { handleGetProfile } from './routes/auth'
-
 const server = fastify()
 
 
@@ -18,8 +20,27 @@ server.listen({ port: 7979 }, (err, address) => {
   console.log(`Server listening at ${address}`)
 })
 
-interface IQuerystring {
+
+declare module 'fastify' {
+  interface FastifyRequest {
+    user?: {
+      id: number
+      pseudo: string
+      email: string
+      token: string | null
+    }
+  }
+}
+
+interface ILogin {
+  log_name: string;
+  password: string;
+}
+
+interface ISignin {
+  pseudo: string;
   mail: string;
+  password: string;
 }
 
 interface IHeaders {
@@ -33,22 +54,38 @@ interface IReply {
 }
 
 server.get<{
-  Querystring: IQuerystring,
+  Querystring: ILogin,
   Headers: IHeaders,
   Reply: IReply
 }>('/auth', {
-  preValidation: async (request, reply, done) => {
-	// parsing
-	// verif api key
-	// code
-    const { mail } = request.query
-    await handleGetProfile(mail, reply).then(() => done()).catch((err) => {
-		console.error('Error in handleGetProfile:', err);
-		reply.code(400).send({ error: 'Internal Server Error' });
-	});
+	preValidation: async (request, reply) => {
+	const { log_name, password } = request.query
+	const user = await getProfile(log_name, password, reply)
+	request.user = user
   }
 }, async (request, reply) => {
   const customerHeader = request.headers['h-Custom']
-  // do something with request data
+  const updatedUser = await prisma.user.update({
+	where: { id: request.user!.id },
+	data: { token: await generateToken() }
+  })
+  console.log(updatedUser)
+  return { success: true }
+})
+
+
+server.get<{
+  Querystring: ISignin,
+  Headers: IHeaders,
+  Reply: IReply
+}>('/signin', {
+	preValidation: async (request, reply) => {
+	const { pseudo, mail, password } = request.query;
+	await signinValidations(pseudo, mail, password, reply);
+  }
+}, async (request, reply) => {
+  const customerHeader = request.headers['h-Custom']
+  const newUser = await createUser(request.query.pseudo, request.query.mail, request.query.password, reply)
+  console.log(newUser)
   return { success: true }
 })
