1b8fe9511e912bef29ae16e45b764167fecd98a6
abosc <achillebosc.dev@icloud.com>
2025-12-28 17:54:56 +0100

    feat(Structure): :fire: Edit the structure of code for a clean WebSocket handling
    I add le most important modules in package.json dans create initial types en constants

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,12 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.ts                                            :+:      :+:    :+:   */
+/*   const.ts                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abosc <abosc@student.42lehavre.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/17 20:07:48 by abosc             #+#    #+#             */
-/*   Updated: 2025/12/17 20:07:49 by abosc            ###   ########.fr       */
+/*   Created: 2025/12/22 14:46:01 by abosc             #+#    #+#             */
+/*   Updated: 2025/12/22 14:46:01 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+import { kongHandler }	from "../Kong/GamesManager";
+import { aowHandler }	from "../AgeOfWar/GamesManager";
+
+export const playerConnections: Map<string, any>	= new Map();
+export const PORT: number							= process.env['PORT'] ? parseInt(process.env['PORT']) : 3000;
+
+export const HANDLERS = {
+    kong:	kongHandler,
+    aow:	aowHandler,
+    auth:	auth,
+};
