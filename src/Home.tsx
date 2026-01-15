51c1a7e29d63a56641f5515b182f9821d8bfb1a0
Pika337 <amelie.dubois337@gmail.com>
2026-01-15 14:46:19 +0100

    Front :
    Start of the Chat's page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,33 @@
-import './Home.css'
+import '../node_modules/tailwindcss/index.css';
 
 function Home () {
 	
 	return (
 		<>
-			coucou
+			<div className="bg-[#1E1E1E] text-[#6E3CA3]">
+				<div className="min-h-screen flex items-center justify-center">
+					<div className="w-1/2 text-center p-4 sm:p-5">
+						The Game's name1 <br />
+						<div className="flex justify-center items-center">
+							<a href="/"><input
+								type="submit"
+								value="Register"
+								className="w-full sm:w-auto p-2 px-5 text-sm sm:text-base bg-linear-[90deg,#6E3CA3,#A82828] focus:outline-2 text-[#FFFFFF]"
+								/></a>
+						</div>
+					</div>
+					<div className="w-1/2 text-center p-4 sm:p-5">
+						The Game's name2 <br />
+						<div className="flex justify-center items-center">
+							<a href="/"><input
+								type="submit"
+								value="Register"
+								className="w-full sm:w-auto p-2 px-5 text-sm sm:text-base bg-linear-[90deg,#6E3CA3,#A82828] focus:outline-2 text-[#FFFFFF]"
+								/></a>
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,5 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./public/index.html",
   ],
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'violet': '#6E3CA3',
-      'red': '#A82828',
-      'blanc': '#ffffffff',
-      'jsp': '#282828',
-    }
-  },
   plugins: [],
 };
