284dd8ec4b6f9f2302fb06f34ade55325975887b
alegrix <16al07@gmail.com>
2026-01-16 16:16:53 +0100

    Api friendly Api requirement + split of signin fonction

--- src_serveur/routes/signin.ts
+++ src_serveur/routes/signin.ts
@@ -2,34 +2,58 @@ import { findProfile } from '../../src/script'
 import { prisma } from '../../prisma'
 import { hashPassword, comparePassword } from '../utils/hashing'
 
-export async function signinValidations(pseudo: string, mail: string, password: string, reply: any)
+export async function signinValidations(pseudo: string, email: string, password: string, reply: any)
 {
-	if (password.length < 8)
+	if (!pseudo || !email || !password)
+	{
+		reply.code(404).send({ error: 'All field are required' })
+		return false
+	}
+	else if (password.length < 8)
 	{
 		reply.code(404).send({ error: 'Password need more than 8 charactere' })
-		return
+		return false
 	}
-	if (!mail || mail.length === 0 || mail.includes(' ') ||
-		mail.split('@').length !== 2 || !mail.includes('.'))
+	else if (!email || email.length === 0 || email.includes(' ') ||
+		email.split('@').length !== 2 || !email.includes('.'))
 	{
 		reply.code(404).send({ error: 'Enter a valid email address' })
-		return
+		return false
 	}
-	if (!pseudo || pseudo.length === 0 || pseudo.includes(' ') || pseudo.includes('@'))
+	else if (!pseudo || pseudo.length === 0 || pseudo.includes(' ') || pseudo.includes('@'))
 	{
 		reply.code(404).send({ error: 'bad pseudo' })
-		return
+		return false
 	}
+	const existing = await prisma.user.findFirst({
+    where: {
+      OR: [
+        { pseudo },
+        { email }
+      ]
+    }
+	});
+	if (existing)
+    {
+		reply.code(404).send({ error: 'Pseudo or email already used' })
+		return false
+	};
+	return true
 }
 
-export async function createUser(pseudo: string, mail: string, password: string, reply: any)
+export async function createUser(pseudo: string, email: string, password: string, reply: any)
 {
 	const newUser = await prisma.user.create({
 	data: {
 		pseudo: pseudo,
-		email: mail,
+		email: email,
 		password: await hashPassword(password),
 	},
+	select: {
+      id: true,
+      pseudo: true,
+      email: true
+    }
 	})
 	return newUser
 }
