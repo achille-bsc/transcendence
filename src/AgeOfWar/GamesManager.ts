b4d2594fc7d2be2211f17475f758bc0fbe6ca4db
achille-bsc <achillebosc.dev@icloud.com>
2026-01-25 15:29:34 +0100

    fix(GamesManager): :bug: correct file header and update function parameters

--- src/AgeOfWar/GamesManager.ts
+++ src/AgeOfWar/GamesManager.ts
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.ts                                            :+:      :+:    :+:   */
+/*   GamesManager.ts                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abosc <abosc@student.42lehavre.fr>         +#+  +:+       +#+        */
+/*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 20:07:48 by abosc             #+#    #+#             */
-/*   Updated: 2025/12/17 20:07:49 by abosc            ###   ########.fr       */
+/*   Updated: 2026/01/12 13:30:32 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import { ClientState, WSMessage } from "../utils/types";
 import { WebSocket } from "@fastify/websocket";
 
-export function aowHandler(webSocket: WebSocket, msg: WSMessage, state: ClientState)
+export function aowHandler(_webSocket: WebSocket, _msg: WSMessage, _state: ClientState)
 {
 
 }
