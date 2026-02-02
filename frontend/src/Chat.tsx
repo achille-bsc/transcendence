bbc28bb7f3442aa74300b8ac769fbe6fcc3785b8
Pika337 <amelie.dubois337@gmail.com>
2026-02-02 16:34:41 +0100

    Frontend (Update) :
    Github button but the button doesn't work.

--- frontend/src/Chat.tsx
+++ frontend/src/Chat.tsx
@@ -4,20 +4,20 @@ import { verifToken } from "./script/utils";
 function Chat () {
 
 	let isAuth = false;
-	const token = localStorage.getItem('token')
-	if (token)
-	{
-		if (!verifToken(token))
-		{
-			window.location.href = "/log";
-		}
-		else
-		{
-				isAuth = true;
-		}
-	}
-	else
-		window.location.href = "/log";
+	// const token = localStorage.getItem('token')
+	// if (token)
+	// {
+	// 	if (!verifToken(token))
+	// 	{
+	// 		window.location.href = "/log";
+	// 	}
+	// 	else
+	// 	{
+	// 			isAuth = true;
+	// 	}
+	// }
+	// else
+	// 	window.location.href = "/log";
 
 	const [msg, setMsg] = useState("");
 
