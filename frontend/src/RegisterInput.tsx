d8267c7b6bc3d20f63aac1e6795847bd25a28294
Pika337 <amelie.dubois337@gmail.com>
2026-02-03 17:25:47 +0100

    Front (update):
    Add more components in the actual code.

---
+++ frontend/src/RegisterInput.tsx
@@ -0,0 +1,31 @@
+interface InputProps {
+	type?: string;
+	id?:string;
+	name?:string;
+	value?: string;
+	className?: string;
+	placeholder?: string;
+	onChange?: () => void;
+	autoComplete?:string;
+	required?:boolean;
+}
+
+export default function RegisterInput ({value, type, id, name, className, placeholder, onChange, autoComplete, required} : InputProps) {
+
+	return (
+		<div className="w-full">
+			{placeholder}
+			<input
+				type={type}
+				id={id}
+				name={name}
+				value={value}
+				className={className}
+				placeholder={placeholder}
+				onChange={onChange}
+				autoComplete={autoComplete}
+				required={required}
+				/>
+		</div>
+	)
+}
