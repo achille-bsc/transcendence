f449dcdd516153be4c89410409d8731649b055fb
alegrix <16al07@gmail.com>
2026-03-02 17:14:09 +0100

    data-pre-split (Fix) : can start now :)
    Push correction of data-base service

--- database-service/src_serveur/routes/health.ts
+++ database-service/src_serveur/routes/health.ts
@@ -4,7 +4,7 @@ import { hashPassword } from '../utils/hashing';
 
 export default async function healthRoutes(server: FastifyInstance) {
   server.get('/health', async () => {
-    const tester = await prisma.user.create({
+    /*const tester = await prisma.user.create({
       data: {
         pseudo: 'healthcheck',
         email: 'healthcheck@test.com',
@@ -18,7 +18,7 @@ export default async function healthRoutes(server: FastifyInstance) {
       return { status: 'error' };
     if (test!.id !== tester.id)
       return { status: 'error' };
-    await prisma.user.deleteMany({ where: { id: tester.id } });
+    await prisma.user.deleteMany({ where: { id: tester.id } });*/
     return { status: 'ok' };
   });
 }
