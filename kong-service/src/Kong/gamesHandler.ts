7cadc8041f7095a7f0077b518421b3ddd3de8821
achille-bsc <achillebosc@gmail.com>
2026-03-09 14:28:45 +0100

    fix(Gameplay): :bug: Fix collisions bugs, players speed & gravity force
    impove moves & phisics, resolve collisions problemes with barils and floor

--- kong-service/src/Kong/gamesHandler.ts
+++ kong-service/src/Kong/gamesHandler.ts
@@ -6,15 +6,37 @@
 /*   By: abosc <abosc@student.42lehavre.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 16:27:36 by marvin            #+#    #+#             */
-/*   Updated: 2026/03/01 03:42:59 by abosc            ###   ########.fr       */
+/*   Updated: 2026/03/08 17:18:05 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-import { games, GRAVITY, JUMP_FORCE, kongMaxHeight, MOVE_SPEED, persoKongHeight }	from "../utils/const";
-import { ClientState, Game, Difficults, KongGameState, PlayerDatas }				from "../utils/types";
-import { generateKongMap, getPlatformYAtX }											from "./kongMap";
-import { sleep }																	from "../utils/utils";
-
+import { 
+	BARREL_FALL_SPEED,
+	BARREL_HALF_SIZE,
+	PLAYER_HALF_W,
+	PLAYER_HALF_H,
+	DIST_FOR_WIN,
+	games,
+	GRAVITY,
+	IMMOBILIZED_TIME,
+	JUMP_FORCE,
+	kongMaxHeight,
+	kongMaxLength,
+	MOVE_SPEED,
+	persoKongHeight,
+	persoKongWidth,
+	SECONDS_BEFORE_START
+}								from "../utils/const";
+import {
+	ClientState,
+	Game,
+	Difficults,
+	KongGameState,
+	PlayerDatas
+}	from "../utils/types";
+import { MAP, getPlatformYAtX }	from "./kongMap";
+import { sleep }				from "../utils/utils";
+import { randomInt }			from "crypto";
 
 export function handleGame(state: ClientState, datas:
 	[
@@ -22,10 +44,16 @@ export function handleGame(state: ClientState, datas:
 		| "joinGame"
 		| "jump"
 		| "goLeft"
-		| "goRight",
+		| "goRight"
+		| "goDown"
+		| "startGame",
 		string,
 		Difficults,
-	]
+		"local" | "online"
+	],
+	localUser: {
+		id: string,
+	}
 ): void
 {
 	// if (!state.gameId)
@@ -36,108 +64,213 @@ export function handleGame(state: ClientState, datas:
 	const game: Game | undefined = games.get(state.gameId!);
 	if (!game)
 		return ;
-
-	switch (datas[0]) {
-		case 'jump':
-			jump(game, state.id!);
-			break;
-	
-		case 'goLeft':
-			goLeft(game, state.id!);
-			break;
+	else
+	{
+		switch (datas[0]) {
+			case 'jump':
+				jump(game, state.id!, localUser);
+				break;
 		
-		case 'goRight':
-			goRight(game, state.id!);
-			break;
-
-		default:
-			break;
+			case 'goLeft':
+				goLeft(game, state.id!, localUser);
+				break;
+			
+			case 'goRight':
+				goRight(game, state.id!, localUser);
+				break;
+				
+			case 'goDown':
+				goDown(game, state.id!, localUser);
+				break;
+	
+			default:
+				break;
+		}
 	}
 }
 
-function jump(game: Game, playerId: string): void
+function jump(game: Game, playerId: string, localUser: { id: string }): void
 {
-	console.log(`Player ${playerId} request Jumping`)
-	const player = game.players.get(playerId);
-	if (!player || player.isJumping /*|| !player.isOnGround*/)
-		return;
-	console.log(`Player jumping`);
+	const player = game.players.get(game.isLocal ? localUser.id : playerId);
+	if (!player || player.isJumping || player.isGoingSpawn/*|| !player.isOnGround*/)
+			return;
 	player.isJumping = true;
 	player.isOnGround = false;
 	player.velocityY = JUMP_FORCE;
 }
 
-function goLeft(game: Game, playerId: string): void
+function goLeft(game: Game, playerId: string, localUser: { id: string }): void
 {
-	console.log(`Player ${playerId} is trying to go left in game hosted by ${game.host}`);
-	const player = game.players.get(playerId);
-	if (!player)
+	const player = game.players.get(game.isLocal ? localUser.id : playerId);
+	if (!player || player.isGoingSpawn)
 		return ;
 	player.isGoingLeft = true;
-	// game.players.delete(playerId);
-	// game.players.set(playerId, player);
 }
 
-function goRight(game: Game, playerId: string): void
+function goRight(game: Game, playerId: string, localUser: { id: string }): void
 {
-	const player = game.players.get(playerId);
-	if (!player)
+	const player = game.players.get(game.isLocal ? localUser.id : playerId);
+	if (!player || player.isGoingSpawn)
 		return ;
 	player.isGoingRight = true;
-	// game.players.delete(playerId);
-	// game.players.set(playerId, player);
 }
 
+function goDown(game: Game, playerId: string, localUser: { id: string }): void
+{
+	const player = game.players.get(game.isLocal ? localUser.id : playerId);
+	if (!player || !player.isOnGround || !game.map || player.isGoingSpawn)
+		return ;
+	if (player.x > 150 && player.x < game.map.platforms[0]!.endX - 150)
+		return ;
+	player.isGoingDown = true;
+}
 
 export function startGame(game: Game)
 {
 	// map de 5 etages
-	game.map = generateKongMap(5);
-
+	game.map = MAP;
 	game.players.forEach((player) => {
-		console.log(`Initializing player ${player.id} in game hosted by ${game.host}`);
-		player.x = game.map!.spawnPoint.x;
-		player.y = game.map!.spawnPoint.y;
+		player.x = game.map?.spawnPoint.x || 1000;
+		player.y = game.map?.spawnPoint.y || 1000;
 		player.velocityY = 0;
 		player.isOnGround = false;
 	});
 
-	console.log(`Starting game hosted by ${game.host}`);
-	console.log(`Map generated with ${game.map.platforms.length} platforms`);
-	game.isStarted = true;
 	sendGameState(game, true);
 	gameLoop(game);
+	setTimeout(() => {
+		game.isStarted = true;
+	}, SECONDS_BEFORE_START * 1000);
 }
+
 
 async function gameLoop(game: Game)
 {
+	let frameBeforeBaril = 20;
 	let frameCount = 0;
 
-	while (!game.isFinish && game.isStarted) {
-		game = games.get(game.id.toString())!;
-		if (!game || !game.map)
-			break;
+	while (!game.isFinish) {
+		if (game.isStarted) {
+			game = games.get(game.id.toString())!;
+			if (!game || !game.map)
+				break;
 
-		game.players.forEach((player) => {
-			if (player.isGoingLeft) {
-				player.x -= MOVE_SPEED;
-				player.isGoingLeft = false;
-			}
-			if (player.isGoingRight) {
-				player.x += MOVE_SPEED;
-				player.isGoingRight = false;
-			}
-			player.x = Math.max(80, Math.min(player.x, game.map!.platforms[0]!.endX));
-		});
-
-		handleGamePhysics(game);
+			game.players.forEach((player) => {
+				if (player.isGoingLeft) {
+					let i = 0;
+					while (i < 6)
+					{
+						player.x -= MOVE_SPEED;
+						player.isGoingLeft = false;
+						checkPlayerCollisionsWithBarils(player, game);
+						i++;
+					}
+				}
+				if (player.isGoingRight) {
+					let i = 0;
+					while (i < 6)
+					{
+						player.x += MOVE_SPEED;
+						player.isGoingRight = false;
+						checkPlayerCollisionsWithBarils(player, game);
+						i++;
+					}
+				}
+				player.x = Math.max(persoKongWidth / 2, Math.min(player.x, kongMaxLength - persoKongWidth / 2));
+			});
+		}
+		
+		await sleep(50);
 		frameCount++;
-		await sleep(100);
+		if (frameCount >= frameBeforeBaril || randomInt(0, 1000) < 10) {
+			frameCount = 0;
+			barilsGeneration(game);
+			frameBeforeBaril = randomInt(15, 30);
+		}
+		handlePlayersPhysics(game);
+		if (game.isStarted) {
+			checkForBarilCollisions(game);
+			checkForWin(game);
+		}
 		sendGameState(game);
 	}
 }
 
-function sendGameState(game: Game, includeMap: boolean = false)
+function isCollidingWithBaril(player: PlayerDatas, baril: { x: number, y: number }): boolean
+{
+	const collDistX = PLAYER_HALF_W + BARREL_HALF_SIZE;
+	const collDistY = PLAYER_HALF_H + BARREL_HALF_SIZE;
+	return Math.abs(player.x - baril.x) < collDistX
+		&& Math.abs(player.y - baril.y) < collDistY;
+}
+
+function respawnPlayer(player: PlayerDatas, game: Game): void
+{
+	if (player.isGoingSpawn)
+		return ;
+	player.x = game.map!.spawnPoint.x;
+	player.y = game.map!.spawnPoint.y;
+	player.velocityY = 0;
+	player.isOnGround = false;
+	player.isGoingSpawn = true;
+	setTimeout(() => {
+		player.isGoingSpawn = false;
+	}, IMMOBILIZED_TIME);
+}
+
+function checkPlayerCollisionsWithBarils(player: PlayerDatas, game: Game)
+{
+	game.barils.forEach((baril) => {
+		if (isCollidingWithBaril(player, baril))
+			respawnPlayer(player, game);
+	});
+}
+
+function checkForBarilCollisions(game: Game)
+{
+	game.barils.forEach((baril) => {
+		game.players.forEach((player) => {
+			if (isCollidingWithBaril(player, baril))
+				respawnPlayer(player, game);
+		});
+	});
+}
+
+function checkForWin(game: Game)
+{
+	game.players.forEach((player) => {
+		const diffX = player.x - game.map!.goalPoint.x;
+		const diffY = player.y - game.map!.goalPoint.y;
+
+		if (diffX < DIST_FOR_WIN && diffX > -DIST_FOR_WIN && diffY < 0 && diffY > -DIST_FOR_WIN)
+		{
+			game.isFinish = true;
+			sendGameState(game);
+		}
+	});
+
+	if (game.isFinish) {
+		const playerIds = Array.from(game.players.keys());
+		playerIds.forEach(id => game.players.delete(id));
+		games.delete(game.id.toString());
+	}
+}
+
+function barilsGeneration(game: Game)
+{
+	if (!game.map)
+		return;
+	
+	if (game.barils.size >= 28)
+		return;
+	game.barils.set(`baril_${Date.now()}`, {
+		x: game.map.spawnPoint.x -500,
+		y: 0,
+		id: `baril_${Date.now()}`
+	});
+}
+
+export function sendGameState(game: Game, includeMap: boolean = false)
 {
 	const mapData = (includeMap && game.map) ? {
 		platforms: game.map.platforms,
@@ -156,8 +289,10 @@ function sendGameState(game: Game, includeMap: boolean = false)
 		
 		const message: KongGameState = {
 			type: 'gameState',
-			players: playersData
+			players: playersData,
+			barils: Object.fromEntries(game.barils),
 		};
+		
 		
 		if (mapData) {
 			message.map = mapData;
@@ -167,52 +302,147 @@ function sendGameState(game: Game, includeMap: boolean = false)
 	});
 }
 
-// function handleGameFphysics(game: Game)
-// {
-// 	if (!game.map)
-// 		return;
-	
-// 	game.players.forEach((player) => {
-// 		if (!player.isOnGround && player.velocityY < 0)
-// 		{
-// 			player.velocityY += GRAVITY;
-// 		}
-// 	})
-// }
+function handleBarilsPhysics(game: Game)
+{
+	const toDelete: string[] = [];
+	game.barils.forEach((baril) => {
+		if (baril.y > kongMaxHeight + 50)
+		{
+			toDelete.push(baril.id);
+			return ;	
+		}
+		let targetPlatformY = Infinity;
+		let platformUnderBaril = game.map!.platforms[0]!;
 
-function handleGamePhysics(game: Game)
+		for (const platform of game.map!.platforms)
+		{
+			if (baril.x < platform.startX || baril.x > platform.endX)
+				continue;
+			
+			const platformY = getPlatformYAtX(platform, baril.x);
+			if (platformY >= baril.y && platformY < targetPlatformY)
+			{
+				platformUnderBaril = platform;
+				targetPlatformY = platformY;
+			}
+		}
+		if (targetPlatformY < Infinity)
+		{
+			if (targetPlatformY - baril.y <= BARREL_FALL_SPEED)
+				baril.y = targetPlatformY;
+			else
+				baril.y += BARREL_FALL_SPEED;
+		}
+		else
+			baril.y += BARREL_FALL_SPEED;
+
+		if (baril.y == targetPlatformY)
+		{
+			baril.x += platformUnderBaril.fallOnRight ? BARREL_FALL_SPEED : -BARREL_FALL_SPEED;
+			baril.y = getPlatformYAtX(platformUnderBaril, baril.x);
+		}
+
+		if ((baril.x > platformUnderBaril.endX - 50) || (baril.x < platformUnderBaril.startX + 50))
+			baril.y += 10;
+	});
+}
+
+function handlePlayersPhysics(game: Game)
 {
 	if (!game.map)
 		return;
 
-	game.players.forEach((player) => {
-		console.log(`appplying gravity for ${player.id} with velocity ${player.velocityY}`)
-		if (!player.isOnGround && player.velocityY < 0)
-			player.velocityY += GRAVITY;
-		
-		player.y += player.velocityY;
-		player.isOnGround = false;
-		
-		for (const platform of game.map!.platforms) {
-			if (player.x < platform.startX || player.x > platform.endX)
-				continue;
+	handleBarilsPhysics(game);
 
-			const platformY = getPlatformYAtX(platform, player.x);
-			const playerFeetY = player.y + persoKongHeight / 2;
-			
-			if (playerFeetY >= platformY && playerFeetY <= platformY + 15 && player.velocityY >= 0) {
-				player.y = platformY - persoKongHeight / 2;
+	game.players.forEach((player) => {
+		if (player.isOnGround) {
+			let stayOnPlatform = false;
+			for (const platform of game.map!.platforms) {
+				if (player.x < platform.startX || player.x > platform.endX)
+					continue;
+				const platformY = getPlatformYAtX(platform, player.x);
+				const feetY = player.y + persoKongHeight / 2;
+				if (Math.abs(feetY - platformY) <= 20) {
+					player.y = platformY - persoKongHeight / 2;
+					stayOnPlatform = true;
+					break;
+				}
+			}
+			if (!stayOnPlatform)
+				player.isOnGround = false;
+		}
+
+		if (player.isGoingDown)
+		{
+			player.y += 10;
+			player.isGoingDown = false;
+			player.isOnGround = false;
+		}
+
+		if (!player.isOnGround) {
+			player.velocityY += GRAVITY;
+			const previousY = player.y;
+			player.y += player.velocityY;
+
+			// Head collision (block player from passing through platform from below)
+			if (player.velocityY < 0 && (player.x > 150 && player.x < game.map!.platforms[0]!.endX - 150)) {
+				let bestPlatformY = -1;
+				// let actualPlatform = game.map!.platforms[0]!;
+				for (const platform of game.map!.platforms) {
+					if (player.x < platform.startX - 50 || player.x > platform.endX)
+						continue;
+					const platformY = getPlatformYAtX(platform, player.x);
+					const prevHeadY = previousY - persoKongHeight / 2;
+					const currHeadY = player.y - persoKongHeight / 2;
+
+					if (prevHeadY >= platformY && currHeadY <= platformY) {
+						if (platformY > bestPlatformY)
+						{
+							bestPlatformY = platformY;
+							// actualPlatform = platform;
+						}
+					}
+				}
+
+				if (bestPlatformY > -1) {
+					player.y = bestPlatformY + 8 * persoKongHeight;
+					player.velocityY = 0;
+				}
+			}
+
+			// Feet collision (land on platform from above)
+			let bestPlatformY = Infinity;
+			for (const platform of game.map!.platforms) {
+				if (player.x < platform.startX || player.x > platform.endX)
+				{
+					player.isOnGround = false;					
+					continue;
+				}
+				const platformY = getPlatformYAtX(platform, player.x);
+				const prevFeetY = previousY + persoKongHeight / 2;
+				const currFeetY = player.y + persoKongHeight / 2;
+
+				if (prevFeetY <= platformY && currFeetY >= platformY && player.velocityY > 0) {
+					if (platformY < bestPlatformY)
+						bestPlatformY = platformY;
+				}
+			}
+
+			if (bestPlatformY < Infinity) {
+				player.y = bestPlatformY - persoKongHeight / 2;
 				player.velocityY = 0;
 				player.isOnGround = true;
 				player.isJumping = false;
-				break;
 			}
 		}
+
 		
+
 		if (player.y > kongMaxHeight + 50) {
 			player.x = game.map!.spawnPoint.x;
 			player.y = game.map!.spawnPoint.y;
 			player.velocityY = 0;
+			player.isOnGround = false;
 		}
 	});
 }
--- kong-service/src/utils/types.ts
+++ kong-service/src/utils/types.ts
@@ -17,6 +17,7 @@ export type Platform = {
   endY:      number,
   hasLadder: boolean,
   ladderX:   number,
+  fallOnRight: boolean,
 }
 
 export type KongMap = {
@@ -41,6 +42,7 @@ export type KongGameState = {
     spawnPoint: { x: number, y: number },
     goalPoint: { x: number, y: number },
   }
+  barils: Record<string, Baril>
 }
 
 export type WSMessage = 
@@ -59,45 +61,60 @@ export type WSMessage =
             | 'jump'
             | 'goLeft'
             | 'goRight'
+            | 'startGame'
           ),
           string,
           Difficults,
-        ]
+          "local" | "online"
+        ],
+        Localuser: {
+          id: string,
+        }
       }
     }
   | {
-      type: 'auth',
-      userID: string
-      payload: { token: string }
+      type:         'auth',
+      userID:       string
+      payload:      { token: string }
     }
 ;
 export type ClientState = {
-  id?:				string,
+  id?:				      string,
   isAuthenticated:	boolean,
-  lastPingAt:		number,
-  gameId:			string | null
+  lastPingAt:		    number,
+  gameId:			      string | null,
+  localPlayerId?:   string,
 }
 
 export type PlayerDatas = {
-  id:				string,
-  x:				number,
-  y:				number,
-  velocityY:		number,			// Vitesse verticale (gravité)
-  isGoingLeft?:		boolean,
-  isGoingRight?:	boolean,
-  isJumping?:		boolean,
-  isOnGround?:		boolean,		// Est sur une plateforme
-  socket:			WebSocket,
+  id:				        string,
+  x:				        number,
+  y:				        number,
+  velocityY:		    number,
+  isGoingLeft?:		  boolean,
+  isGoingRight?:	  boolean,
+  isJumping?:		    boolean,
+  isOnGround?:		  boolean,
+  isGoingDown?:	    boolean,
+  isGoingSpawn?:	    boolean,
+  socket:			      WebSocket,
+}
+
+export type Baril = {
+    x: number,
+    y: number,
+    id: string,
 }
 
 export type Game = {
-  host:				string,
-  id:         number,
-  difficulty:		Difficults,
-  players_count:	number,
-  players:			Map<string, PlayerDatas>,
-  map?:				KongMap,		// Map du niveau
-  isFinish:			boolean
-  isStarted:		boolean
-  barils:			{ x: number, y: number }[]
+  host:			    	  string,
+  id:               number,
+  difficulty:	  	  Difficults,
+  players_count:	  number,
+  players:			    Map<string, PlayerDatas>,
+  barils:			      Map<string, Baril>
+  map?:		    		  KongMap,
+  isFinish:		  	  boolean,
+  isStarted:	  	  boolean,
+  isLocal:		  	  boolean,
 }
