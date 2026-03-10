9b72fd88869959dbb9d7ecf07f13d838f52aad68
alegrix <16al07@gmail.com>
2026-03-10 22:52:54 +0100

    Front/back (fix) : Receive Route
    Now, this branch is up to date with all modification of the front and back

--- frontend/src/utils/Main.tsx
+++ frontend/src/utils/Main.tsx
@@ -261,8 +261,7 @@ function Main({children = ""}: {children?: ReactNode}) {
 
 	useEffect(() => {
 		fetchPending().then(result => setPending(result || []));
-		
-		// Rafraîchir les demandes en attente toutes les 5 secondes
+		console.log("Pending friends:", pending);		
 		const interval = setInterval(() => {
 			fetchPending().then(result => setPending(result || []));
 		}, 5000);
