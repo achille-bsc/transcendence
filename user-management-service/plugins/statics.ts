f448954b5fb4ac56b4a996680cdddd97c120838c
Pika337 <amelie.dubois337@gmail.com>
2026-03-11 16:43:26 +0100

    Frontend (update):
    Fix prblm and email and apikey can be used

--- user-management-service/plugins/statics.ts
+++ user-management-service/plugins/statics.ts
@@ -1,11 +1,10 @@
 import fastifyStatic from '@fastify/static';
-import path from 'path';
-import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
+import { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
 
 async function setupStaticFiles(server: FastifyInstance) {
   server.register(fastifyStatic, {
-    root: '/app/default_avatars',
+    root: '/app/avatars',
     prefix: '/public/',
   });
 }
